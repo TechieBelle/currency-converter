724bf3b0978b30da2fd73b0f1e46e5fdaa678031
Folashade Bello <folashadebello.b@gmail.com>
2025-08-28 12:53:05 +0100

    React and Tailwind configuration

---
+++ daily-rate/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+ 
+
+  return (
+    <>
+      <div class="bg-sky-400">
+        <h1 class="text-3xl font-bold ">Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App
